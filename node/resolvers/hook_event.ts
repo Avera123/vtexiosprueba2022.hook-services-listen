f04f63a23e3607243b7921306ab5e42bd095b62c
Alejandro Vera Carrasquilla <avera02@misena.edu.co>
2024-10-17 09:48:16 -0500

    Nuevo Hook Listener

---
+++ node/resolvers/hook_event.ts
@@ -0,0 +1,4 @@
+
+const urlBase = "https://vtexiosprueba2022.myvtex.com/api/dataentities"
+const urlEntityHookEvents = urlBase + "/hook_events/documents"
+const urlBaseDocumentsById = urlEntityHookEvents + "/{$id}"
